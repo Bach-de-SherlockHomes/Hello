e3097907d634c0a6cb45f352564692e120bccefb
Bach-de-SherlockHomes <vietbach1601@gmail.com>
2024-09-20 17:01:28 +0700

    Phương

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào Phùng Khánh Phương',
+    introDesc: `Em biết tại sao có cái này không???`,
+    btnIntro: 'Click vào đây má',
+    title: 'Hãy trả lời câu hỏi dưới đây để bị phạt',
+    desc: 'Em Phương có yêu anh Bách không',
+    btnYes: 'Có yêu',
+    //btnNo: 'Không,em chỉ coi anh là daddy Bách',
+    question:'Sao em lại yêu anh',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Trả lời ở đây má',
+    mess: 'Biết ngay, anh yêu vợ nhiều lắm =))))))',
+    messDesc: 'Tối nay 19h30 anh qua đón nhé công chúa.',
+    btnAccept: 'Click tiếp vào đây má',
+    messLink: 'https://www.youtube.com/watch?v=W2FRMzCuPzY' 
+}
